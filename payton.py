c9da94c96a68b5b506bf135d0ded836f4f8b8731
ROBOTMAR621 <59299944+ROBOTMAR621@users.noreply.github.com>
2025-02-05 21:23:58 -0600

    Add files via upload

---
+++ payton.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import random
+
+
+def mover_boton():
+
+    nueva_x = random.randint(0, 500) 
+    nueva_y = random.randint(0, 300)  
+
+  
+    boton.place(x=nueva_x, y=nueva_y)
+
+
+ventana = tk.Tk()
+ventana.title("el boton mas pro ")
+
+
+ventana.geometry("600x400") 
+
+
+boton = tk.Button(ventana, text="click si no sos gay", command=mover_boton, bg="blue", fg="white", font=("Arial", 14), width=20, height=3)
+boton.place(x=100, y=150)  
+
+
+ventana.mainloop()
